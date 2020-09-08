85e5f19e5f9ffbcd8bf61163cbe333fe90ec7fba
darrenzel <darrenbian@cmail.carleton.ca>
2020-09-08 00:37:11 -0400

    IOS style calculator webapp

---
+++ calculator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
